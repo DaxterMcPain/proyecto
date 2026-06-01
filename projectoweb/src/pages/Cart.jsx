73378a3c28463ce54d2257c2b822d0548f9007cc
DaxterMcPain <yamil-aldana-izquierdo@hotmail.com>
2026-06-01 09:31:20 -0500

    Actualizando Carrito de compras

--- projectoweb/src/pages/Cart.jsx
+++ projectoweb/src/pages/Cart.jsx
@@ -1,28 +1,35 @@
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
 function Cart() {
 
-    const cart =
-        JSON.parse(localStorage.getItem("cart")) || [];
+    const [cart, setCart] = useState(
+        JSON.parse(localStorage.getItem("cart")) || []
+    );
+    const removeProduct = (indexToRemove) => {
+        const newCart = cart.filter(
+            (_, index) => index !== indexToRemove
+        );
+        setCart(newCart);
+        localStorage.setItem(
+            "cart",
+            JSON.stringify(newCart)
+        );
+    };
 
     return (
         <div className="catalog-container">
-
             <h1>Carrito de Compras</h1>
-
             {cart.length === 0 ? (
                 <p>No hay productos en el carrito.</p>
             ) : (
-                <div className="products-grid">
 
+                <div className="cart-list">
                     {cart.map((product, index) => (
-
                         <div
                             className="product-card"
                             key={index}
                         >
                             <h3>{product.name}</h3>
-
                             <p>
                                 Categoría: {product.category}
                             </p>
@@ -30,13 +37,18 @@ function Cart() {
                             <p>
                                 S/ {product.price}
                             </p>
+
+                            <button
+                                onClick={() =>
+                                    removeProduct(index)
+                                }
+                            >
+                                Eliminar
+                            </button>
                         </div>
-
                     ))}
-
                 </div>
             )}
-
             <br />
 
             <Link to="/">
